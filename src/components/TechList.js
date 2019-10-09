51adc46f6db2e8bf11c87c7d041f88dd67ef7d5a
RafaelM <RafaelM.Science@gmail.com>
2019-10-09 15:18:50 -0400

    feat delete
    - foi adicionado para deletar o item

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
+import Techitem from "./Techitem";
 
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "ReactJS", "React Native"]
   };
 
-  render() {
-    console.log(this.state);
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    /*
+     *  o objeto é imutavel no caso ele criar um novo array
+     * ... ele pega todo e copia e depois ele adiciona como ultima posicao
+     * ele criar um novo em vez de mudar pois o estado e imutavel
+     * newtech:"" para deixar ele vazio
+     */
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  };
+
+  render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <Techitem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
---
+++ src/components/Techitem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Techitem({ tech, onDelete }) {
+  return (
+    <li>
+      {tech}
+      <button onClick={onDelete} type="button">
+        Remover
+      </button>
+    </li>
+  );
+}
+
+export default Techitem;
